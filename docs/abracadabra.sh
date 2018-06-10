d62f38b44c0770c3b652211be12be542855376a1
hippogriff <gopalsharma1193@gmail.com>
2018-06-10 12:01:05 -0400

    includes link to 3D repository

---
+++ docs/abracadabra.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo  ==============================================================================
+echo  :: Cayman Theme :: updating website.
+pandoc  --no-highlight \
+        --lua-filter=task-list.lua \
+        --from       markdown_github+smart+yaml_metadata_block+auto_identifiers \
+        --to         html5 \
+        --template   ./cayman.template \
+        --output     ./index.html \
+        ../README.md ./configuration.yaml
